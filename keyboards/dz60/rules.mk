fb2abd9d6d790101fad89be53e22d8d1801fe7dd
Michal Havlát <michal.havlat@kajot.cz>
2024-10-24 14:54:21 +0200

    huh

---
+++ keyboards/dz60/rules.mk
@@ -0,0 +1,20 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no
+COMBO_ENABLE = no #new      # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no     # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+AVR_USE_MINIMAL_PRINTF = yes
+LTO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+LEADER_ENABLE = no # new
+MIDI_ENABLE = no
+UNICODE_ENABLE = no # might zapnout
+
